2927d4dc4cf57ae190a86bf295442a54f0b6423f
Sai Nihal <109018562+Nihalrt@users.noreply.github.com>
2023-08-03 10:53:04 -0700

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "ojqdttxmprgsfbpvvvtq.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
