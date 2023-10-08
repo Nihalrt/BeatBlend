394655524f30c512d2ba167707d8baa0bb5e593d
Sai Nihal <109018562+Nihalrt@users.noreply.github.com>
2023-10-08 12:09:41 -0700

    Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -151,7 +151,7 @@ const Header: React.FC<HeaderProps> = ({
               <div>
                 <Button 
                   onClick={authModal.onOpen} 
-                  className="bg-white px-6 py-2"
+                  className="bg-black px-6 py-2"
                 >
                   Log in
                 </Button>
